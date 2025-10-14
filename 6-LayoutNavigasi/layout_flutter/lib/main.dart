f5ceb43eb5492c3253843e40cce13731719b5829
VozSoldat <muhamaderril7@gmail.com>
2025-10-14 13:13:06 +0700

    p6: implementasi button row

--- 6-LayoutNavigasi/layout_flutter/lib/main.dart
+++ 6-LayoutNavigasi/layout_flutter/lib/main.dart
@@ -40,14 +40,48 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Nama dan NIM Anda',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Muhammad Erril - 2341720183')),
-        body: titleSection,
+        body: Column(
+          children: [
+            titleSection,
+            buttonSection
+          ],
+        )
       ),
     );
   }
 
-  
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
